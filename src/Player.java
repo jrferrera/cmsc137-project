a170f5e4ff1056e7884cd309a3673546c3a96c20
jrferrera <jrferrera17@gmail.com>
2014-11-09 12:05:43 +0800

    Added player class

---
+++ src/Player.java
@@ -0,0 +1,47 @@
+import java.net.InetAddress;
+
+
+public class Player {
+	private String username;
+	private int port;
+	private InetAddress address;
+	private ChatBox chatBox;
+	
+	public Player(String username, int port, InetAddress address) {
+		this.setUsername(username);
+		this.setPort(port);
+		this.setAddress(address);
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public InetAddress getAddress() {
+		return address;
+	}
+
+	public void setAddress(InetAddress address) {
+		this.address = address;
+	}
+
+	public ChatBox getChatBox() {
+		return chatBox;
+	}
+
+	public void setChatBox(ChatBox chatBox) {
+		this.chatBox = chatBox;
+	}
+}
