ea4931ecd7a52f3e2db471911b7fffb53e79f58b
jrferrera <jrferrera17@gmail.com>
2014-11-11 23:00:22 +0800

    Removed unused import

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,7 +4,6 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
