f9a7cd4416b38e7a99364b3a10eda131404b7c80
jrferrera <jrferrera17@gmail.com>
2014-11-12 12:40:03 +0800

    Added message sending

--- src/ChatBox.java
+++ src/ChatBox.java
@@ -14,16 +14,16 @@ import javax.swing.JTextField;
 public class ChatBox extends JPanel implements ActionListener, KeyListener {
 	private JTextArea messageBox;
 	private JTextField textfield;
+	private JLabel usernameLabel;
 	private JButton sendButton;
-	private JLabel username;
 	private JScrollPane scrollbar;
 	
 	//UI, Player Name, Host Name
-	public ChatBox() {
+	public ChatBox(String username) {
 		messageBox = new JTextArea(10,10);
 		textfield = new JTextField();
+		usernameLabel = new JLabel(username);
 		sendButton = new JButton("Send");
-		username = new JLabel("Username");
 		scrollbar = new JScrollPane(messageBox, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		
 		messageBox.setFocusable(false);
@@ -36,19 +36,26 @@ public class ChatBox extends JPanel implements ActionListener, KeyListener {
 		add(scrollbar, BorderLayout.NORTH);
 		add(sendButton, BorderLayout.EAST);
 		add(textfield, BorderLayout.SOUTH);
-		add(username, BorderLayout.WEST);
+		add(usernameLabel, BorderLayout.WEST);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
-		messageBox.append(username.getText() + ": " + textfield.getText()+ "\n");
-		textfield.setText("");
+		sendMessage();
 	}
 	
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
-			messageBox.append(username.getText() + ": " + textfield.getText()+ "\n");
-			textfield.setText("");
+			sendMessage();
 		}
+	}
+	
+	public void sendMessage() {
+		GameUtility.sendToServer("CHAT_ALL|" + "chatMessage=" + textfield.getText());
+	}
+	
+	public void appendMessage(String message) {
+		messageBox.append(message + "\n");
+		textfield.setText("");
 	}
 	
 	public void	keyReleased(KeyEvent e){ }
