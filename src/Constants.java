ab28be203d5c6d73f4fb5e3e0461510a42899054
jrferrera <jrferrera17@gmail.com>
2014-11-12 12:40:15 +0800

    Added server value

--- src/Constants.java
+++ src/Constants.java
@@ -3,6 +3,9 @@ public interface Constants {
 	// Game Name
 	public static final String GAME_TITLE = "Game";
 	
+	// Server
+	public static final String SERVER = "localhost";
+	
 	// Host
 	public static final String HOST = "127.0.0.1";
 		
@@ -17,6 +20,7 @@ public interface Constants {
 	
 	// Game State Flags
 	public static final int WAITING_FOR_PLAYERS = 1;
+	public static final int START_BATTLE = 2;
 	
 	//Minimum Number of Players
 	public static final int MINIMUM_PLAYER_COUNT = 1;
