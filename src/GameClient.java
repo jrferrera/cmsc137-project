5767cc294fec224c887ecfe0ded493ec90bd7348
jrferrera <jrferrera17@gmail.com>
2014-11-12 12:37:08 +0800

    Changed Game to GameClient

--- src/GameClient.java
+++ src/GameClient.java
@@ -1,23 +1,25 @@
 import java.awt.Dimension;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 
-public class Game extends JFrame implements Constants, Runnable {
+public class GameClient extends JFrame implements Constants, Runnable {
 	private Thread gameThread;
 	private DatagramSocket clientSocket;
 	private DatagramPacket packet;
 	private byte[] buffer;
 	private String serverData;
-	private int gameStateFlag;
+	private HashMap<String, String> hashData;
 	boolean connected;
 	
-	public Game() {
+	public GameClient() {
 		setTitle(GAME_TITLE);
-		setPreferredSize(new Dimension(GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT));	
 		setMinimumSize(new Dimension(GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
@@ -46,17 +48,27 @@ public class Game extends JFrame implements Constants, Runnable {
 			
 			if(!connected && serverData.startsWith("CONNECTED")) {
 				connected = true;
-				GameUtility.changeScreen(new GamePortal());
-			}else if(!connected && serverData.startsWith("CONNECTION FAILED")) {
-				JOptionPane.showMessageDialog(null, "Connection Failed", "Message", JOptionPane.ERROR_MESSAGE);
-			}else if(connected) {
 				
+				hashData = GameUtility.parser(serverData);
+				InetAddress address;
+				try {
+					address = InetAddress.getByAddress(hashData.get("address"), buffer);
+					GameUtility.player = new Player(hashData.get("username"), Integer.parseInt(hashData.get("port")), address);
+				} catch (UnknownHostException e) { }
+			}else if(!connected && serverData.startsWith("CONNECTION_FAILED")) {
+				JOptionPane.showMessageDialog(null, "Connection Failed", "Message", JOptionPane.ERROR_MESSAGE);
+			}else if(connected && serverData.startsWith("CHAT_ALL")) {
+				hashData = GameUtility.parser(serverData);
+				
+				GameUtility.player.getChatBox().appendMessage(GameUtility.player.getUsername() + ": " + hashData.get("chatMessage"));
+			}else if(connected) {
+				GameUtility.changeScreen(new GamePortal());
 			}
 		}
 	}
 	
 	public static void main(String args[]) {
-		new Game();
+		new GameClient();
 	}
 }
 
