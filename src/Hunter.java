074e4d40c2ef789fc4560486caea827be7066fbf
jrferrera <jrferrera17@gmail.com>
2014-11-19 18:43:28 +0800

    Added character classes

---
+++ src/Hunter.java
@@ -0,0 +1,8 @@
+
+public class Hunter extends Character {
+	public Hunter() {
+		super();	
+		setAttack(13);
+		setDefense(9);
+	}
+}
---
+++ src/Knight.java
@@ -0,0 +1,8 @@
+
+public class Knight extends Character {
+	public Knight() {
+		super();	
+		setAttack(15);
+		setDefense(10);
+	}
+}
