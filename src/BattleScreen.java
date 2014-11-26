8c97a6eb9cc10d2f7b7cff381102515d97e816a5
jrferrera <jrferrera17@gmail.com>
2014-11-26 18:08:14 +0800

    Modified

--- src/BattleScreen.java
+++ src/BattleScreen.java
@@ -4,25 +4,21 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 
-<<<<<<< HEAD:src/BattleScreen.java
 public class BattleScreen extends JPanel implements Constants {
-	public GameClient ui;
-	public Player player;
+	private Player player;
 	private JPanel battlefieldPanel;
+	private GameStatistics gameStatistics;
 	private GameMenu gameMenu;
 	
 	public BattleScreen(Player player) {
-		this.ui = ui;
 		this.player = player;
 		gameMenu = new GameMenu();
 		
 		battlefieldPanel = new JPanel(new BorderLayout());
-		battlefieldPanel.add(gameMenu, BorderLayout.SOUTH);
-=======
-public class Battlefield extends JPanel implements Constants {
-	public Battlefield() {
->>>>>>> origin/jeff:src/Battlefield.java
+		gameStatistics = new GameStatistics(player);
 		
+		battlefieldPanel.add(gameStatistics, BorderLayout.NORTH);
+		battlefieldPanel.add(gameMenu, BorderLayout.SOUTH);
 	}
 	
 	public void paintComponent(Graphics g) {
